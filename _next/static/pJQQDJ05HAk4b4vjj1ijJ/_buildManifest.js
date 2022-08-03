2a8fa7fb66b27fd1184fce391b4b74a004762c30
jowch <jjmaomi@gmail.com>
2022-08-03 03:50:55 +0000

    Deploying to gh-pages from @ jowch/jowch.github.io@b157849554d77e823987e6236b49ac1f44349829 🚀

---
+++ _next/static/pJQQDJ05HAk4b4vjj1ijJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/365-865e7796238e3dfe.js","static/chunks/pages/index-b2a232ec0b9421a8.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
